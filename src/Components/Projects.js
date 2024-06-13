291ca2d2ffa2a00e4957c1ad5bab7d2475fc8cfb
pascoa <pascoasekgota27@gmail.com>
2024-06-13 17:02:19 +0200

    .

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -15,23 +15,20 @@ const Projects = () => {
         
         <div className="project__frame1">
        <h1>Explore My Work</h1>
-       <p>View my github account for more <br/> projects<PiProjectorScreenChartLight/> </p>
+       <p>View my github account for more projects<PiProjectorScreenChartLight/> </p>
        <button className='project__btnG rounded-full ' onClick={github}>
-        <SlSocialGithub /> Github</button>
+        <SlSocialGithub /></button>
        </div>
        <div className='project__projects'>
 
         <div  rounded-lg> <iframe className="project__frame4"
         title="Example"
-        // width="395"
-        // height="415"
         src="https://pnsyildiz.github.io/Dronoticz/"
         allowFullScreen>
         </iframe></div>
        <div> <iframe className="project__frame2"
         title="Example"
-        // width="560"
-        // height="415"
+
         src="https://pnsyildiz.github.io/lifestyle/"
         allowFullScreen
       ></iframe></div>
